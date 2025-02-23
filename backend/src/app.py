c78e02372142f5dac73122c9453776188981711d
金城茉紘 <fuduki@kinjounoMacBook-Air.local>
2025-02-23 09:43:48 +0900

    setup build config

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import io
-import librosa
-import soundfile as sf
 from .analyze_voice import analyze_voice
 
 app = FastAPI()
@@ -18,28 +16,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.post("/predict")
-# async def predict(file: UploadFile = File(...)):
-#     print("aaaa")
-#     try:
-#         # 音声データを読み込む
-#         audio_bytes = await file.read()
-#         audio_stream = io.BytesIO(audio_bytes)
-
-#         try:
-#             # soundfileで読み込み
-#             audio_data, sr = sf.read(audio_stream)
-#         except:
-#             # soundfileで失敗した場合、librosaを試す
-#             audio_stream.seek(0)
-#             audio_data, sr = librosa.load(audio_stream, sr=None)
-
-#         # 仮の分類結果（AIモデルに置き換える予定）
-#         result = {"osaka": 0.2, "kyoto": 0.5, "hyogo": 0.3, "other": 0.0}
-
-#         return {"success": True, "result": result}
-#     except Exception as e:
-#         return {"success": False, "error": str(e)}
 
 # 音声データを受け取り、AI判定を行うエンドポイント
 @app.post("/upload")
@@ -50,12 +26,7 @@ async def upload_audio(request: Request):
         print(f"受け取った音声データの長さ: {len(audio_data)} バイト")
         audio_stream = io.BytesIO(audio_data)
         
-        ##audio_data, sr = sf.read(audio_stream)
-        # audio_stream.seek(0)
-        # with open("./test.wav", 'wb') as f:
-        #     f.write(audio_data)
-        # print(audio_stream)
-        # audio_data, sr = librosa.load(audio_stream, sr=None)
+       
         audio_data, sr =torchaudio.load(audio_stream)
         audio_data=audio_data.detach().cpu().numpy()
         if audio_data.shape[1]/sr<5:
