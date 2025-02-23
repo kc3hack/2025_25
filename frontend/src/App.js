29ec585418a35bcf95e5f0b25dc899a08fd96307
2412110044 <nishibori1618@gmail.com>
2025-02-23 06:40:02 +0000

    create ReadingScript.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Logo from './components/Logo';
+import ReadingScript from "./components/ReadingScript";
 import AudioRecorder from './components/AudioRecorder';
 import ResultField from './components/ResultField';
 import "./App.css";
 
-const scripts = [
-  "こんにちは！", "天気がいいですね", "今日は何をしますか？", "好きな食べ物は何ですか？",
-  "旅行に行きたい場所は？", "最近読んだ本は？", "趣味は何ですか？", "週末の予定は？"
-];
-
 function App() {
   const [result, setResult] = useState(null);
-  const [displayScripts, setDisplayScripts] = useState([]);
-
-  useEffect(() => {
-    // 3つのランダムなスクリプトを選ぶ
-    const shuffled = [...scripts].sort(() => 0.5 - Math.random()).slice(0, 3);
-    setDisplayScripts(shuffled);
-  }, []);
+  const [recording, setRecording] = useState(false);
 
   return (
     <div className="App">
@@ -26,19 +16,12 @@ function App() {
 
         <h1>出身地判定アプリ</h1>
         
-        <p>読んでみてください：</p>
-        {/* ランダムなスクリプトを図形の中に表示 */}
-        <div className="script-container">
-          {displayScripts.map((script, index) => (
-            <div key={index} className="script-box">{script}</div>
-          ))}
-        </div>
+        <ReadingScript recording={recording} />
+        <br />
         
         {/* 録音コンポーネント */}
-        <AudioRecorder onReceiveResponse={setResult} />
-        <AudioRecorder onReceiveResponse={setResult}/>
-
-        <br></br>
+        <AudioRecorder onReceiveResponse={setResult} setRecording={setRecording} />
+        <br />
 
         {/* 判定結果を出力 */}
         <ResultField result={result} />
---
+++ frontend/src/components/ReadingScript.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const scripts = [
+    "こんにちは！", "天気がいいですね", "今日は何をしますか？", "好きな食べ物は何ですか？",
+    "旅行に行きたい場所は？", "最近読んだ本は？", "趣味は何ですか？", "週末の予定は？"
+];
+  
+const ReadingScript = (props) => {
+    const [displayScripts, setDisplayScripts] = useState([]);
+
+    const recording = props.recording;
+
+    useEffect(() => {
+        // 3つのランダムなスクリプトを選ぶ
+        const shuffled = [...scripts].sort(() => 0.5 - Math.random()).slice(0, 3);
+        setDisplayScripts(shuffled);
+    }, []);
+    
+    return (<>
+        <p>読んでみてください：</p>
+        {/* ランダムなスクリプトを図形の中に表示 */}
+        <div className="script-container">
+            {displayScripts.map((script, index) => (
+                <div key={index} className="script-box">{script}</div>
+            ))}
+        </div>
+    </>)
+};
+
+export default ReadingScript;
